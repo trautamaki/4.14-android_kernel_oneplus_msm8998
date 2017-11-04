1876eef21654861c30c1bb660e1ab23178d7809b
Angelo G. Del Regno <kholk11@gmail.com>
2017-11-04 12:37:42 +0100

    soc: qcom: memshare: Don't call hypervisor mappings on MSM8916 class
    MSM8916-class SoC's TZ do not support hypervisor mappings.

--- drivers/soc/qcom/memshare/msm_memshare.c
+++ drivers/soc/qcom/memshare/msm_memshare.c
@@ -413,6 +413,7 @@ static struct notifier_block nb = {
 	.notifier_call = modem_notifier_cb,
 };
 
+#ifndef CONFIG_ARCH_MSM8916
 static void shared_hyp_mapping(int client_id)
 {
 	int ret;
@@ -438,6 +439,7 @@ static void shared_hyp_mapping(int client_id)
 	}
 	memblock[client_id].hyp_mapping = 1;
 }
+#endif
 
 static void handle_alloc_generic_req(struct qmi_handle *handle,
 	struct sockaddr_qrtr *sq, struct qmi_txn *txn, const void *decoded_msg)
@@ -502,6 +504,7 @@ static void handle_alloc_generic_req(struct qmi_handle *handle,
 	memblock[client_id].alloc_request = 1;
 
 	fill_alloc_response(alloc_resp, client_id, &resp);
+#ifndef CONFIG_ARCH_MSM8916
 	/*
 	 * Perform the Hypervisor mapping in order to avoid XPU viloation
 	 * to the allocated region for Modem Clients
@@ -509,6 +512,7 @@ static void handle_alloc_generic_req(struct qmi_handle *handle,
 	if (!memblock[client_id].hyp_mapping &&
 		memblock[client_id].allotted)
 		shared_hyp_mapping(client_id);
+#endif
 	mutex_unlock(&memsh_drv->mem_share);
 	dev_info(memsh_child->dev,
 		"memshare_alloc: client_id: %d, alloc_resp.num_bytes: %d, alloc_resp.resp.result: %lx\n",
