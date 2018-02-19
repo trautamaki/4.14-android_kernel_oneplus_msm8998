716ef44e5c3cf101c6cd67cea511133b1bb822da
Robin Murphy <robin.murphy@arm.com>
2018-02-19 13:38:00 +0000

    UPSTREAM: arm64: uaccess: Formalise types for access_ok()
    In converting __range_ok() into a static inline, I inadvertently made
    it more type-safe, but without considering the ordering of the relevant
    conversions. This leads to quite a lot of Sparse noise about the fact
    that we use __chk_user_ptr() after addr has already been converted from
    a user pointer to an unsigned long.
    
    Rather than just adding another cast for the sake of shutting Sparse up,
    it seems reasonable to rework the types to make logical sense (although
    the resulting codegen for __range_ok() remains identical). The only
    callers this affects directly are our compat traps where the inferred
    "user-pointer-ness" of a register value now warrants explicit casting.
    
    Bug: 112461694
    
    Change-Id: I4b28f0542ff3a242387c5be9c144c1f204edd240
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    (cherry picked from commit 9085b34d0e8361595a7d19034c550d5d15044556)

--- arch/arm64/include/asm/uaccess.h
+++ arch/arm64/include/asm/uaccess.h
@@ -72,9 +72,9 @@ static inline void set_fs(mm_segment_t fs)
  * This is equivalent to the following test:
  * (u65)addr + (u65)size <= (u65)current->addr_limit + 1
  */
-static inline unsigned long __range_ok(unsigned long addr, unsigned long size)
+static inline unsigned long __range_ok(const void __user *addr, unsigned long size)
 {
-	unsigned long limit = current_thread_info()->addr_limit;
+	unsigned long ret, limit = current_thread_info()->addr_limit;
 
 	/*
 	 * Asynchronous I/O running in a kernel thread does not have the
@@ -89,7 +89,7 @@ static inline unsigned long __range_ok(unsigned long addr, unsigned long size)
 	asm volatile(
 	// A + B <= C + 1 for all A,B,C, in four easy steps:
 	// 1: X = A + B; X' = X % 2^64
-	"	adds	%0, %0, %2\n"
+	"	adds	%0, %3, %2\n"
 	// 2: Set C = 0 if X > 2^64, to guarantee X' > C in step 4
 	"	csel	%1, xzr, %1, hi\n"
 	// 3: Set X' = ~0 if X >= 2^64. For X == 2^64, this decrements X'
@@ -101,9 +101,9 @@ static inline unsigned long __range_ok(unsigned long addr, unsigned long size)
 	//    testing X' - C == 0, subject to the previous adjustments.
 	"	sbcs	xzr, %0, %1\n"
 	"	cset	%0, ls\n"
-	: "+r" (addr), "+r" (limit) : "Ir" (size) : "cc");
+	: "=&r" (ret), "+r" (limit) : "Ir" (size), "0" (addr) : "cc");
 
-	return addr;
+	return ret;
 }
 
 #define access_ok(type, addr, size)	__range_ok((unsigned long)(addr), size)
