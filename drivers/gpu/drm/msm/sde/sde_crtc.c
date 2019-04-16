a2321a81fa6435a4b34ba50bb6be57053158962e
Shubhashree Dhar <dhar@codeaurora.org>
2019-04-16 21:44:07 +0530

    drm/msm/sde: validate plane ROI to be within layer mixer boundary
    Add check in sde display for plane destination rectangle
    so that it falls within the range of layer mixer out size.
    
    Change-Id: I51108c1f88e6e6f89cb0ac4df886bd5daad42221
    Signed-off-by: Shubhashree Dhar <dhar@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -5414,6 +5414,15 @@ static int sde_crtc_atomic_check(struct drm_crtc *crtc,
 
 		cnt++;
 
+		if ((pstate->crtc_h > mixer_height)
+				|| (pstate->crtc_w > mixer_width)) {
+			SDE_ERROR("plane w/h:%x*%x more than mixer w/h:%x*%x\n",
+					pstate->crtc_w, pstate->crtc_h,
+					mixer_width, mixer_height);
+			rc = -E2BIG;
+			goto end;
+		}
+
 		if (CHECK_LAYER_BOUNDS(pstate->crtc_y, pstate->crtc_h,
 				mode->vdisplay) ||
 		    CHECK_LAYER_BOUNDS(pstate->crtc_x, pstate->crtc_w,
