1bb50ce8ce6d5a6c9ca926b5524b18c8ae759240
Angelo G. Del Regno <kholk11@gmail.com>
2019-05-23 19:11:56 +0200

    irqchip: qcom: mpm: Add MPM drivers for MSM8956, 8996, 8998, 630, 660
    Add a snapshot of the MPM drivers for MSM8956, MSM8996, MSM8998,
    SDM630, SDM636/660 from the current Open Devices kernel 4.9 HEAD.

--- drivers/irqchip/qcom/mpm-sdm630.c
+++ drivers/irqchip/qcom/mpm-sdm630.c
@@ -13,20 +13,18 @@
 
 #include "mpm.h"
 
-const struct mpm_pin mpm_sdm660_gic_chip_data[] = {
+const struct mpm_pin mpm_sdm630_gic_chip_data[] = {
 	{ 0x02,	216 },	 /* tsens1_tsens_upper_lower_int */
+	{ 0x31,	212 },	 /* usb30_power_event_irq */
 	{ 0x34,	275 },	 /* qmp_usb3_lfps_rxterm_irq_cx */
 	{ 0x3d,	209 },	 /* lpi_dir_conn_irq_apps[1] */
-	{ 0x4f,	379 },	 /* qusb2phy_intr for Dm */
-	{ 0x50,	380 },	 /* qusb2phy_intr for Dm for secondary PHY */
-	{ 0x51,	379 },	 /* qusb2phy_intr for Dp */
-	{ 0x52,	380 },	 /* qusb2phy_intr for Dp for secondary PHY */
+	{ 0x4f,	379 },	 /* qusb2phy_intr */
 	{ 0x57,	358 },	 /* ee0_apps_hlos_spmi_periph_irq */
 	{ 0x5b,	519 },	 /* lpass_pmu_tmr_timeout_irq_cx */
 	{-1},
 };
 
-const struct mpm_pin mpm_sdm660_gpio_chip_data[] = {
+const struct mpm_pin mpm_sdm630_gpio_chip_data[] = {
 	{ 3, 1 },
 	{ 4, 5 },
 	{ 5, 9 },
