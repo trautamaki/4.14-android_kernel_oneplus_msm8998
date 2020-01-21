c7477cd5597a27ba16c2db1c62490eddb9721e66
Nathan Chancellor <natechancellor@gmail.com>
2020-01-20 19:24:28 -0700

    Revert "lib/genalloc.c: use vzalloc_node() to allocate the bitmap"
    This reverts commit d84f622a917d3737dcaedf9c981e660e6bfdb19b.
    
    This causes Yoshino to fail to boot, as discovered with a git bisect.
    I do not see any real reason why this should happen but at the same
    time, Qualcomm devices are probably not subject to the issue that the
    commit notes.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- lib/genalloc.c
+++ lib/genalloc.c
@@ -189,7 +189,7 @@ int gen_pool_add_virt(struct gen_pool *pool, unsigned long virt, phys_addr_t phy
 	unsigned long nbytes = sizeof(struct gen_pool_chunk) +
 				BITS_TO_LONGS(nbits) * sizeof(long);
 
-	chunk = vzalloc_node(nbytes, nid);
+	chunk = kzalloc_node(nbytes, GFP_KERNEL, nid);
 	if (unlikely(chunk == NULL))
 		return -ENOMEM;
 
@@ -253,7 +253,7 @@ void gen_pool_destroy(struct gen_pool *pool)
 		bit = find_next_bit(chunk->bits, end_bit, 0);
 		BUG_ON(bit < end_bit);
 
-		vfree(chunk);
+		kfree(chunk);
 	}
 	kfree_const(pool->name);
 	kfree(pool);
