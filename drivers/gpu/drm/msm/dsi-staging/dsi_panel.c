cf58570d1baec83c833ef3f70dfc13ac592713b5
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-05-04 11:45:14 +0200

    drm: msm: Implement codepath variations for cont-splash DMS
    On some devices we want to change the display resolution during
    continuous splash - before the userspace kicks in -.
    
    Implement code path variations in order to seamlessly change
    the resolution of the display and accordingly set the DSI PHY:
    this way, we get a smooth transition from the continuous splash
    logo to the first frame that's being sent from the userspace.
    
    To achieve this, add a new property and, based on finding that,
    temporarily disallow pushing frames, then force reprogramming
    the DSI PHY registers with the new timings while in ON state
    and carefully also reset the PPS settings (for the eventual
    Display Stream Comnpression display).
    
    Resume accepting any frame only after the setup is completely
    over, so that we're sure that no image corruption happens in
    the event of the userspace sending them before.
    In the event of a very long init process, the frames will be
    dropped on expiry, instead of semaphore waiting forever.
    
    This behavior gets activated by setting the property
    "somc,splash-dms-switch-to-this-timing" on the specific
    timing that is meant to be target of this DMS during splash
    mechanism.
    
    Example:
    disp-timing@0 {
    	timings....
    };
    disp-timing@1 {
    	somc,splash-dms-switch-to-this-timing;
    	timings....
    };

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -3996,6 +3996,17 @@ int dsi_panel_get_mode(struct dsi_panel *panel,
 		if (rc)
 			pr_err("failed to partial update caps, rc=%d\n", rc);
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+#ifdef CONFIG_DRM_SDE_SPECIFIC_PANEL
+		mode->isDefault = of_property_read_bool(child_np,
+				"qcom,mdss-dsi-timing-default");
+
+		mode->splash_dms = of_property_read_bool(child_np,
+				"somc,splash-dms-switch-to-this-timing");
+#endif /* CONFIG_DRM_SDE_SPECIFIC_PANEL */
+>>>>>>> 0e3cc8ab94cb (drm: msm: Implement codepath variations for cont-splash DMS)
 
 		/*
 		 * No support for pixel overlap in DSC enabled or Partial
@@ -4634,12 +4645,14 @@ int dsi_panel_switch(struct dsi_panel *panel)
 		return -EINVAL;
 	}
 
+	pr_info("Sending resolution switch command.\n");
+
 	mutex_lock(&panel->panel_lock);
 
 	rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_TIMING_SWITCH);
 	if (rc)
-		pr_err("[%s] failed to send DSI_CMD_SET_TIMING_SWITCH cmds, rc=%d\n",
-		       panel->name, rc);
+		pr_err("[%s] failed to send DSI_CMD_SET_TIMING_SWITCH cmds,"
+		       " rc=%d\n", panel->name, rc);
 
 	mutex_unlock(&panel->panel_lock);
 	return rc;
