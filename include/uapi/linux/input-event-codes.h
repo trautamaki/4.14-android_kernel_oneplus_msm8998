sonyxperiadev/kernel

input: ts: sec_ts: Cleanup and build fixes
This driver is so, so, so dirty.
My eyes are bleeding.
That's as much of a cleanup as I can do, right now.

P.S.: We require some new event codes (because reasons) so
add them to get the driver to actually build and work.
This thing will get properly cleaned up sooner or later.

include/uapi/linux/input-event-codes.h
@@ -766,6 +766,7 @@
 #define ABS_MT_TOOL_X		0x3c	/* Center X tool position */
 #define ABS_MT_TOOL_Y		0x3d	/* Center Y tool position */
 
+#define ABS_MT_CUSTOM		0x3e	/* custom event */
 
 #define ABS_MAX			0x3f
 #define ABS_CNT			(ABS_MAX+1)
@@ -796,6 +797,7 @@
 #define SW_HPHR_OVERCURRENT	0x11  /* set = over current on right hph */
 #define SW_MICROPHONE2_INSERT	0x12  /* set = inserted */
 #define SW_UNSUPPORT_INSERT	0x13  /* set = unsupported device inserted */
+#define SW_GLOVE		0x14  /* set = glove mode -- ORIGINAL WAS 0x0f*/
 #define SW_MAX			0x20
 #define SW_CNT			(SW_MAX+1)
 

trautamaki/4.14-android_kernel_oneplus_msm8998

include/uapi/linux/input-event-codes.h
@@ -775,6 +775,7 @@
 #define ABS_MT_TOOL_X		0x3c	/* Center X tool position */
 #define ABS_MT_TOOL_Y		0x3d	/* Center Y tool position */
 
+#define ABS_MT_CUSTOM		0x3e	/* custom event */
 
 #define ABS_MAX			0x3f
 #define ABS_CNT			(ABS_MAX+1)
@@ -806,6 +807,7 @@
 #define SW_MICROPHONE2_INSERT	0x12  /* set = inserted */
 #define SW_UNSUPPORT_INSERT	0x13  /* set = unsupported device inserted */
 #define SW_MACHINE_COVER	0x14  /* set = cover closed */
+#define SW_GLOVE		0x15  /* set = glove mode -- ORIGINAL WAS 0x0f*/
 #define SW_MAX			0x20
 #define SW_CNT			(SW_MAX+1)
 